a6be07e25d5dc9350ecc376d2266ee6edb045bf5
karolissilaika-arch <karolissilaika@gmail.com>
2026-02-25 21:09:10 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,107 @@
 const recipes = [
     { 
         title: "Tikka masala", 
-        ingredients: ["vištiena", "kokosų pienas", "svogūnai"], 
+        ingredients: ["vištiena", "kokosų pienas", "svogūnai", "garam masala"], 
         link: "recipes/tikka-masala.html",
         laikas: "3 h", sudetingumas: "Vidutiniškai", 
         img: "https://chefbook-prod.s3.us-west-2.amazonaws.com/1766927860073-1a04bd66dc9152",
-        category: "Indiška", desc: "Klasikinis indiškas patiekalas."
+        category: "Indiška", desc: "Klasikinis indiškas patiekalas su sodriu padažu."
     },
     { 
         title: "Česnakinio sviesto bulvės", 
-        ingredients: ["bulvės", "sviestas", "česnakas"], 
+        ingredients: ["bulvės", "sviestas", "česnakas", "petražolės"], 
         link: "recipes/bulves.html",
-        laikas: "1 h", sudetingumas: "Lengvai", 
-        img: "../bulves.jpg",
-        category: "Garnyras", desc: "Traškios bulvytės su česnakiniu sviestu."
+        laikas: "35 min", sudetingumas: "Lengvai", 
+        img: "https://images.unsplash.com/photo-1518977676601-b53f02ac6d31?auto=format&fit=crop&w=800&q=80",
+        category: "Garnyras", desc: "Traškios bulvytės su aromatingu česnakiniu sviestu."
     }, 
     { 
         title: "Khinkaliai", 
-        ingredients: ["faršas", "miltai", "kiaušinis"], 
+        ingredients: ["faršas", "miltai", "kiaušinis", "kuminas"], 
         link: "recipes/khinkaliai.html",
         laikas: "2 h", sudetingumas: "Vidutiniškai", 
-        img: "../khinkaliai.jpg",
-        category: "Sakartvelo", desc: "Sultingi tradiciniai koldūnai."
+        img: "https://images.unsplash.com/photo-1626128665085-483747621778?auto=format&fit=crop&w=800&q=80",
+        category: "Sakartvelo", desc: "Sultingi tradiciniai koldūnai su mėsos sultiniu viduje."
+    },
+    { 
+        title: "Tikroji Carbonara", 
+        ingredients: ["spaghetti", "kiaušiniai", "šoninė", "sūris", "pipirai"], 
+        link: "recipes/carbonara.html",
+        laikas: "20 min", sudetingumas: "Vidutiniškai", 
+        img: "../carbonara.jpg",
+        category: "Itališka", desc: "Tikras romėniškas skonis be jokios grietinėlės."
+    },
+    { 
+        title: "Česnakinė Naan duona", 
+        ingredients: ["miltai", "jogurtas", "mielės", "sviestas", "česnakas"], 
+        link: "recipes/naan.html",
+        laikas: "1 val 20 min", sudetingumas: "Vidutiniškai", 
+        img: "../naan.jpg",
+        category: "Indiška", desc: "Minkšta, puri ir puikiai tinkanti prie troškinių."
+    },
+    { 
+        title: "Benedikto kiaušiniai", 
+        ingredients: ["kiaušiniai", "bandelė", "kumpis", "sviestas", "citrina"], 
+        link: "recipes/benedict.html",
+        laikas: "25 min", sudetingumas: "Sudėtingiau", 
+        img: "../benedict.jpg",
+        category: "Pusryčiai", desc: "Prabangūs pusryčiai su šilkiniu olandišku padažu."
+    },
+    { 
+        title: "Pikantiška Šakšuka", 
+        ingredients: ["kiaušiniai", "pomidorai", "paprika", "svogūnas", "kuminas"], 
+        link: "recipes/shakshuka.html",
+        laikas: "20 min", sudetingumas: "Lengvai", 
+        img: "../shakshuka.jpg",
+        category: "Pusryčiai", desc: "Vieno puodo pusryčiai tiesiai iš Vidurio Rytų."
+    },
+    { 
+        title: "Adžarijos Chačapuri", 
+        ingredients: ["tešla", "sūris", "kiaušinis", "sviestas"], 
+        link: "recipes/chachapuri.html",
+        laikas: "40 min", sudetingumas: "Vidutiniškai", 
+        img: "../chachapuri.jpg",
+        category: "Sakartvelo", desc: "Garsusis sūrio laivelis su skystu tryniu viduryje."
+    },
+    { 
+        title: "Naminis Ramen", 
+        ingredients: ["makaronai", "sultinys", "soja", "kiaušinis", "imbieras"], 
+        link: "recipes/ramenas.html",
+        laikas: "45 min", sudetingumas: "Vidutiniškai", 
+        img: "../ramen.jpg",
+        category: "Azijietiška", desc: "Sotus ir šildantis japoniškos sriubos dubenėlis."
+    },
+    { 
+        title: "Dešrelės tešloje", 
+        ingredients: ["sluoksniuota tešla", "dešrelės", "kiaušinis", "garstyčios"], 
+        link: "recipes/sausage-rolls.html",
+        laikas: "30 min", sudetingumas: "Lengvai", 
+        img: "../sausage-rolls.jpg",
+        category: "Užkandžiai", desc: "Traškus užkandis, kurį dievina visi."
+    },
+    { 
+        title: "Grybų risotas", 
+        ingredients: ["ryžiai", "grybai", "sultinys", "parmezanas"], 
+        link: "recipes/risotas.html",
+        laikas: "40 min", sudetingumas: "Vidutiniškai", 
+        img: "../risotas.jpg",
+        category: "Itališka", desc: "Kreminis itališkas ryžių patiekalas su miško grybais."
+    },
+    { 
+        title: "Kepti ryžiai (Fried Rice)", 
+        ingredients: ["ryžiai", "kiaušiniai", "daržovės", "soja"], 
+        link: "recipes/fried-rice.html",
+        laikas: "15 min", sudetingumas: "Lengvai", 
+        img: "../fried-rice.jpg",
+        category: "Azijietiška", desc: "Greitai paruošiami kiniški kepti ryžiai su kiaušiniu."
+    },
+    { 
+        title: "Chow Mein makaronai", 
+        ingredients: ["makaronai", "vištiena", "kopūstai", "soja"], 
+        link: "recipes/chow-mein.html",
+        laikas: "20 min", sudetingumas: "Lengvai", 
+        img: "../chow-mein.jpg",
+        category: "Azijietiška", desc: "Populiarūs kepinti kiaušininiai makaronai su daržovėmis."
     }
 ];
 
@@ -162,5 +242,4 @@ window.onclick = function(event) {
 // Mobile menu toggle
 document.getElementById('mobile-menu').onclick = () => {
     document.getElementById('nav-content').classList.toggle('active');
-
 };
--- scripts/receptai.js
+++ scripts/receptai.js
@@ -1,5 +1,114 @@
 /* --- GLOBAL DATA: ALL RECIPES --- */
 const allRecipes = {
+    "naan": {
+        title: "Česnakinė Naan duona",
+        baseServings: 4,
+        storageKey: "naan_notes",
+        defaultNote: "Kepkite sausoje keptuvėje, kol pasirodys pūslės!",
+        ingredients: [
+            { name: "Miltai (kvietiniai)", amt: 2, unit: "stikl.", mAmt: 250, mUnit: "g" },
+            { name: "Graikiškas jogurtas", amt: 3, unit: "v.š.", mAmt: 60, mUnit: "g" },
+            { name: "Sausos mielės", amt: 1, unit: "a.š.", mAmt: 7, mUnit: "g" },
+            { name: "Cukrus", amt: 1, unit: "a.š.", mAmt: 5, mUnit: "g" },
+            { name: "Druska", amt: 0.5, unit: "a.š.", mAmt: 3, mUnit: "g" },
+            { name: "Kepimo milteliai", amt: 1, unit: "a.š.", mAmt: 5, mUnit: "g" },
+            { name: "Kepimo soda", amt: 0.5, unit: "a.š.", mAmt: 3, mUnit: "g" },
+            { name: "Lydytas sviestas", amt: 2, unit: "v.š.", mAmt: 30, mUnit: "g" },
+            { name: "Kalendros(smulkintos)", amt: 1, unit: "vnt.", mAmt: 5, mUnit: "g" },
+            { name: "Šiltas vanduo", amt: 100, unit: "ml", mAmt: 100, mUnit: "ml" },
+            { name: "Česnako skiltelės", amt: 2, unit: "vnt.", mAmt: 10, mUnit: "g" }
+        ]
+    },
+    "carbonara": {
+        title: "Tikroji Carbonara",
+        baseServings: 2,
+        storageKey: "carbonara_notes",
+        defaultNote: "Nenaudokite grietinėlės – kremiškumą suteikia kiaušiniai ir sūris.",
+        ingredients: [
+            { name: "Spaghetti makaronai", amt: 200, unit: "g", mAmt: 200, mUnit: "g" },
+            { name: "Guanciale arba šoninė", amt: 100, unit: "g", mAmt: 100, mUnit: "g" },
+            { name: "Kiaušiniai", amt: 2, unit: "vnt.", mAmt: 100, mUnit: "g" },
+            { name: "Pecorino Romano sūris", amt: 50, unit: "g", mAmt: 50, mUnit: "g" },
+            { name: "Juodieji pipirai", amt: 1, unit: "v.š.", mAmt: 5, mUnit: "g" }
+        ]
+    },
+    "benedict": {
+        title: "Benedikto kiaušiniai",
+        baseServings: 2,
+        storageKey: "benedict_notes",
+        defaultNote: "Olandišką padažą plakite lėtai virš garų vonelės.",
+        ingredients: [
+            { name: "Kiaušiniai", amt: 2, unit: "vnt.", mAmt: 100, mUnit: "g" },
+            { name: "Angliška bandelė", amt: 1, unit: "vnt.", mAmt: 60, mUnit: "g" },
+            { name: "Kumpis", amt: 2, unit: "riek.", mAmt: 40, mUnit: "g" },
+            { name: "Sviestas (atšilęs)", amt: 100, unit: "g", mAmt: 100, mUnit: "g" },
+            { name: "Actas (virti)", amt: 1, unit: "v.š.", mAmt: 15, mUnit: "ml" },
+            { name: "Citrinos sultys", amt: 1, unit: "a.š.", mAmt: 5, mUnit: "ml" }
+        ]
+    },
+    "shakshuka": {
+        title: "Pikantiška Šakšuka",
+        baseServings: 2,
+        storageKey: "shakshuka_notes",
+        defaultNote: "Skaniausia su feta ir šviežia duona.",
+        ingredients: [
+            { name: "Pomidorai savo sultyse", amt: 1, unit: "skard.", mAmt: 400, mUnit: "g" },
+            { name: "Kiaušiniai", amt: 3, unit: "vnt.", mAmt: 150, mUnit: "g" },
+            { name: "Paprika", amt: 1, unit: "vnt.", mAmt: 150, mUnit: "g" },
+            { name: "Kuminas", amt: 1, unit: "a.š.", mAmt: 3, mUnit: "g" },
+            { name: "Česnako skiltelės", amt: 2, unit: "vnt.", mAmt: 10, mUnit: "g" }
+        ]
+    },
+    "chachapuri": {
+        title: "Adžarijos Chačapuri",
+        baseServings: 1,
+        storageKey: "chachapuri_notes",
+        defaultNote: "Prieš valgant, išmaišykite trynį ir sviestą sūrio masėje.",
+        ingredients: [
+            { name: "Milti (kvietiniai)", amt: 200, unit: "g", mAmt: 200, mUnit: "g" },
+            { name: "Mozzarella sūris", amt: 150, unit: "g", mAmt: 150, mUnit: "g" },
+            { name: "Feta sūris", amt: 100, unit: "g", mAmt: 50, mUnit: "g" },
+            { name: "Kiaušinio trynys", amt: 1, unit: "vnt.", mAmt: 20, mUnit: "g" },
+            { name: "Sviestas", amt: 1, unit: "gabal.", mAmt: 20, mUnit: "g" },
+            { name: "Vanduo", amt: 100, unit: "ml", mAmt: 100, mUnit: "ml" },
+            { name: "Druska", amt: 1, unit: "a.š.", mAmt: 5, mUnit: "g"},
+            { name: "Cukrus", amt: 0.5, unit: "a.š.", mAmt: 3, mUnit: "g" },
+            { name: "Mielės (sausos)", amt: 1, unit: "a.š.", mAmt: 7, mUnit: "g" }
+        ]
+    },
+    "ramen": {
+        title: "Naminis Ramen",
+        baseServings: 1,
+        storageKey: "ramen_notes",
+        defaultNote: "Makaronus virkite atskirai, kad sultinys neliktų krakmolingas.",
+        ingredients: [
+            { name: "Ramen makaronai", amt: 1, unit: "porc.", mAmt: 150, mUnit: "g" },
+            { name: "Vištienos šlaunelės (be kaulo)", amt: 4, unit: "vnt.", mAmt: 400, mUnit: "g" },
+            { name: "Svogūnas(smulkiai supjaustytas)", amt: 1, unit: "vnt.", mAmt: 100, mUnit: "g" },
+            { name: "Česnakas(smulkiai supjaustytas)", amt: 3, unit: "vnt.", mAmt: 15, mUnit: "g" },
+            { name: "Vištienos sultinys", amt: 500, unit: "ml", mAmt: 500, mUnit: "ml" },
+            { name: "Sojos padažas", amt: 100, unit: "ml", mAmt: 30, mUnit: "ml" },
+            { name: "Rudasis cukrus", amt: 2, unit: "v.š.", mAmt: 30, mUnit: "g" },
+            { name: "Kiaušinis (virtas)", amt: 1, unit: "vnt.", mAmt: 50, mUnit: "g" },
+            { name: "Jauni svogūnai", amt: 1, unit: "sauja", mAmt: 10, mUnit: "g" }
+        ]
+    },
+    "sausage-rolls": {
+        title: "Dešrelės tešloje",
+        baseServings: 4,
+        storageKey: "sausage_notes",
+        defaultNote: "Galite pabarstyti sezamais prieš kepant.",
+        ingredients: [
+            { name: "Sluoksniuota tešla", amt: 1, unit: "pak.", mAmt: 400, mUnit: "g" },
+            { name: "Malta jautiena su kiauliena", amt: 8, unit: "vnt.", mAmt: 400, mUnit: "g" },
+            { name: "Kiaušinio plakinys", amt: 1, unit: "vnt.", mAmt: 50, mUnit: "g" },
+            { name: "Garstyčios", amt: 1, unit: "v.š.", mAmt: 15, mUnit: "g" },
+            { name: "Druska ir pipirai", amt: 1, unit: "a.š.", mAmt: 5, mUnit: "g" },
+            { name: "kiaušinis", amt: 1, unit: "vnt.", mAmt: 50, mUnit: "g" },
+            { name: "Petražolės (smulkintos)", amt: 2, unit: "v.š.", mAmt: 10, mUnit: "g" },
+            { name: "kiaušinis(plaktas)", amt: 1, unit: "vnt.", mAmt: 50, mUnit: "g" },
+        ]
+    },
     "bulves": {
         title: "Česnakinio sviesto bulvės",
         baseServings: 6,
@@ -52,14 +161,71 @@ const allRecipes = {
             { name: "Paprikos milteliai", amt: 2, unit: "v.š.", mAmt: 30, mUnit: "g" },
             { name: "Kumino milteliai", amt: 2, unit: "v.š.", mAmt: 30, mUnit: "g" }
         ]
+    },
+    "risotas": {
+    title: "Grybų risotas",
+    baseServings: 2,
+    storageKey: "risotas_notes",
+    defaultNote: "Svarbiausia – nuolatinis maišymas ir kantrybė pilant sultinį.",
+    ingredients: [
+        { name: "Arborio ryžiai", amt: 200, unit: "g", mAmt: 200, mUnit: "g" },
+        { name: "Grybai (pievagrybiai)", amt: 250, unit: "g", mAmt: 250, mUnit: "g" },
+        { name: "Daržovių sultinys", amt: 700, unit: "ml", mAmt: 700, mUnit: "ml" },
+        { name: "Sviestas", amt: 30, unit: "g", mAmt: 30, mUnit: "g" },
+        { name: "Parmezano sūris", amt: 50, unit: "g", mAmt: 50, mUnit: "g" },
+        { name: "Svogūnas", amt: 1, unit: "vnt.", mAmt: 100, mUnit: "g" },
+        { name: "Baltas vynas (nebūtina)", amt: 50, unit: "ml", mAmt: 50, mUnit: "ml" }
+    ]
+    },
+    "fried-rice": {
+    title: "Kepti ryžiai (Fried Rice)",
+    baseServings: 2,
+    storageKey: "fried_rice_notes",
+    defaultNote: "Geriausia naudoti vakarykščius, šaldytuve pastovėjusius ryžius.",
+    ingredients: [
+        { name: "Virti ryžiai", amt: 400, unit: "g", mAmt: 400, mUnit: "g" },
+        { name: "Kiaušiniai", amt: 2, unit: "vnt.", mAmt: 100, mUnit: "g" },
+        { name: "Svogūnas", amt: 1, unit: "stikl.", mAmt: 150, mUnit: "g" },
+        { name: "Sojos padažas", amt: 3, unit: "v.š.", mAmt: 45, mUnit: "ml" },
+        { name: "Cukrus", amt: 2, unit: "a.š.", mAmt: 10, mUnit: "g" },
+        { name: "Austrių padažas", amt: 1, unit: "v.š.", mAmt: 15, mUnit: "ml" },
+        { name: "Sezamų aliejus", amt: 1, unit: "a.š.", mAmt: 5, mUnit: "ml" },
+        { name: "Česnako skiltelės", amt: 2, unit: "vnt.", mAmt: 10, mUnit: "g" },
+        { name: "Svogūnų laiškai", amt: 1, unit: "sauja", mAmt: 10, mUnit: "g" }
+    ]
+    },
+    "chow-mein": {
+    title: "Chow Mein makaronai",
+    baseServings: 2,
+    storageKey: "chow_mein_notes",
+    defaultNote: "Nekepkite daržovių per ilgai – jos turi likti traškios!",
+    ingredients: [
+        { name: "Kiaušininiai makaronai", amt: 200, unit: "g", mAmt: 200, mUnit: "g" },
+        { name: "Kopūstas (pjaustytas)", amt: 2, unit: "stikl.", mAmt: 150, mUnit: "g" },
+        { name: "Morkos (šiaudeliais)", amt: 1, unit: "vnt.", mAmt: 80, mUnit: "g" },
+        { name: "Sojos padažas", amt: 2, unit: "v.š.", mAmt: 30, mUnit: "ml" },
+        { name: "Austrių padažas", amt: 1, unit: "v.š.", mAmt: 15, mUnit: "ml" },
+        { name: "Vištienos krūtinėlė", amt: 200, unit: "g", mAmt: 200, mUnit: "g" },
+        { name: "Aliejus", amt: 2, unit: "v.š.", mAmt: 20, mUnit: "ml" }
+    ]
     }
 };
 
 /* --- SEARCH INDEX --- */
 const searchItems = [
+    { title: "Česnakinė Naan duona", link: "../recipes/naan.html", category: "Indiška", img: "../naan.jpg" },
+    { title: "Tikroji Carbonara", link: "../recipes/carbonara.html", category: "Itališka", img: "../carbonara.jpg" },
+    { title: "Benedikto kiaušiniai", link: "../recipes/benedict.html", category: "Pusryčiai", img: "../benedict.jpg" },
+    { title: "Pikantiška Šakšuka", link: "../recipes/shakshuka.html", category: "Pusryčiai", img: "../shakshuka.jpg" },
+    { title: "Adžarijos Chačapuri", link: "../recipes/chachapuri.html", category: "Sakartvelo", img: "../chachapuri.jpg" },
+    { title: "Naminis Ramen", link: "../recipes/ramenas.html", category: "Azijietiška", img: "../ramen.jpg" },
+    { title: "Dešrelės tešloje", link: "../recipes/sausage-rolls.html", category: "Užkandžiai", img: "../sausage-rolls.jpg" },
     { title: "Chicken Tikka Masala", link: "../recipes/tikka-masala.html", category: "Indiška", img: "https://chefbook-prod.s3.us-west-2.amazonaws.com/1766927860073-1a04bd66dc9152" },
     { title: "Česnakinio sviesto bulvės", link: "../recipes/bulves.html", category: "Garnyras", img: "https://images.unsplash.com/photo-1518977676601-b53f02ac6d31?auto=format&fit=crop&w=800&q=80" },
-    { title: "Khinkaliai", link: "../recipes/khinkaliai.html", category: "Sakartvelo", img: "https://images.unsplash.com/photo-1626128665085-483747621778?auto=format&fit=crop&w=800&q=80" }
+    { title: "Khinkaliai", link: "../recipes/khinkaliai.html", category: "Sakartvelo", img: "https://images.unsplash.com/photo-1626128665085-483747621778?auto=format&fit=crop&w=800&q=80" },
+    { title: "Grybų risotas", link: "../recipes/risotas.html", category: "Itališka", img: "../risotas.jpg" },
+    { title: "Kepti ryžiai (Fried Rice)", link: "../recipes/fried-rice.html", category: "Azijietiška", img: "../fried-rice.jpg" },
+    { title: "Chow Mein makaronai", link: "../recipes/chow-mein.html", category: "Azijietiška", img: "../chow-mein.jpg" }
 ];
 
 /* --- FUNCTIONS: SEARCH --- */
